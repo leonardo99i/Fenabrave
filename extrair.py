313254fd98476b9af9fe40341a550c0833052955
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-03-20 12:45:15 -0300

    Testando possibilidades

--- extrair.py
+++ extrair.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 link_sp = "http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap="
 abre_pagina = urllib.request.urlopen(link_sp)
-soup = BeautifulSoup(abre_pagina, 'html5lib')
-busca_item = soup.find('strong', attrs='class: b_inf b_dir fndo')
-extrai_valor = busca_item.decompose()
+soup = BeautifulSoup(abre_pagina, 'html.parser')
+busca_item = soup.find('tbody')
+extrai_valor = busca_item.text.split()
 print(busca_item)
