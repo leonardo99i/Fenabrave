8af8681e86a495eb26133a9b3acf15fde6a669f9
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-03-20 23:09:35 -0300

    Refatorando o codigo

--- tabela.py
+++ tabela.py
@@ -1,18 +1,22 @@
-from cgitb import html
-from urllib.request import urlopen
+#Importação das bibliotecas utilizadas
+from cgitb import html #Biblioteca
+from urllib.request import urlopen #Abre a pagina html
 from urllib.error import HTTPError
-from urllib.error import URLError
-from bs4 import BeautifulSoup
+from urllib.error import URLError  #Informa se ocorre erro na requisição
+from bs4 import BeautifulSoup #Biblioteca que vai fazer o web scraping e capturar os dados que queremos
 
 try:
+    #Link do fenabrave para download
     html = urlopen("http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap=")
-except HTTPError as e:
-    print(e)
+    #Check para saber se a página abre ou não
+except HTTPError as html:
+    print(html)
 except URLError:
     print("Servidor fora do ar ou dominio incorreto")
 else:
-    res = BeautifulSoup(html.read(), "html5lib")
-    tags = res.findAll("table", {"class": "TABELA"})
-    for tag in tags:
+    procura = BeautifulSoup(html.read(), "html5lib")
+    extrai = procura.findAll("table", {"class": "TABELA"})
+    for tag in extrai:
         print(tag.getText())
+
     
