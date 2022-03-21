4b9eecef63c1173ce12b21f52d62aedbd3b41a07
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-03-21 09:06:03 -0300

    main

--- extrair.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-from bs4 import BeautifulSoup
-
-link_sp = "http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap="
-abre_pagina = urllib.request.urlopen(link_sp)
-soup = BeautifulSoup(abre_pagina, 'html.parser')
-busca_item = soup.find('tbody')
-extrai_valor = busca_item.text.split()
-print(busca_item)
--- teste_3.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib3
-
-link_sp = "http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap="
-page = urllib3.urlopen(link_sp)
-soup = BeautifulSoup(page, 'html5lib')
-target = soup.find('strong')
-print(target)
