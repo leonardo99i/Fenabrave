9e27aa563be79419cebda4a914a0a4479015bd36
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-03-23 22:54:58 -0300

    Add informações

--- tabela.py
+++ tabela.py
@@ -5,20 +5,11 @@ from urllib.error import HTTPError #Verifica se o link está certo e pede a para
 from urllib.error import URLError  #Informa se ocorre erro na requisição
 from bs4 import BeautifulSoup #Biblioteca que vai fazer o web scraping e capturar os dados que queremos
 import pandas as pd
-
-try:
-    #Link do fenabrave para download
-    html = urlopen("http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap=")
-    #Check para saber se a página abre ou não
-except HTTPError as html:
-    print(html)
-except URLError:
-    print("Servidor fora do ar ou dominio incorreto, verificar link na variavel html")
-else:
-    procura = BeautifulSoup(html.read(), "html5lib")
-    extrai = procura.findAll("table", {"class": "TABELA"})
-    for tag in extrai:
-        print(tag.getText())
-#Tentar rodar no jupyter notebook
-
+import soupsieve
+#Link do fenabrave para download
+html = urlopen("http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Sao%20Paulo&cap=")
+procura = BeautifulSoup(html.read(), "html5lib")
+extrai = procura.findAll("table", {"class": "TABELA"})
+for tag in extrai:
+    print(tag.getText())
     
