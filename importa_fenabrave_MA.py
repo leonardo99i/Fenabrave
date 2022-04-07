ac92232f5d2cf078ed802941c9d0a3e26233210e
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2022-04-07 09:46:48 -0300

    ADICIONANDO TODOS OS ESTADOS

---
+++ importa_fenabrave_MA.py
@@ -0,0 +1,18 @@
+#Importação das lib's utilizadas
+import pandas as pd
+from urllib.request import urlopen
+import html
+from bs4 import BeautifulSoup
+import csv
+
+html = urlopen("http://www.fenabrave.org.br/pdf/informativo/automatico/dadosregionais_novo.asp?id=Maranhao&cap=")
+soup = BeautifulSoup(html.read(), "html5lib")
+extrai_tabela = soup.findAll("table", {"class": "TABELA"})
+for tag in extrai_tabela:
+        table_str = str(extrai_tabela)
+        tabela_pronta = pd.read_html(table_str)[0]
+        print(tabela_pronta)
+
+import io
+with io.open('UF_MA.csv', "w", encoding="utf-8") as file:
+    file.write(str(tabela_pronta))
